d4f1b517f0d7a198ea69804aeb2a9860af8418a6
phpnt <admin@phpnt.com>
2016-09-24 02:21:52 +0500

    first commit

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package   yii2-cms
+ * @author    Yuri Shekhovtsov <shekhovtsovy@yandex.ru>
+ * @copyright Copyright &copy; Yuri Shekhovtsov, lowbase.ru, 2015 - 2016
+ * @version   1.0.0
+ */
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=5.45.124.29;dbname=lowbase',
+    'username' => 'setyes',
+    'password' => 'a748b5rk9',
+    'charset' => 'utf8',
+];
